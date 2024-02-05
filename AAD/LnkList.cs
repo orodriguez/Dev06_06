15f35712e394f9763473bdb1ccd4272d55742ee3
“orodriguez” <omarjavier15@gmail.com>
2024-02-05 16:31:36 -0400

    Assignment Linked List

--- AAD/LnkList.cs
+++ AAD/LnkList.cs
@@ -4,10 +4,7 @@ public class LnkList<T> {
     
     public static LnkList<T> From(params T[] values)
     {
-        var ll = new LnkList<T>();
-        foreach (var value in values)
-            ll.Add(value);
-        return ll;
+        throw new NotImplementedException();
     }
 
     private LnkNode<T>? _head;
@@ -30,71 +27,24 @@ public class LnkList<T> {
     // O(1)
     public void Add(T element)
     {
-        var newNode = new LnkNode<T>(element);
-
-        // O(1)
-        if (_head == null)
-            _head = _last = newNode;
-        else // O(1)
-        {
-            _last.Next = newNode;
-            _last = newNode;
-        }
-        
-        _count++;
+        throw new NotImplementedException();
     }
 
     // O(n)
     public void Insert(int index, T value)
     {
-        // O(1)
-        if (_count == 0)
-            return;
-
-        // O(1)
-        if (index == 0)
-        {
-            var newNode = new LnkNode<T>(value, _head);
-            _head = newNode;
-            return;
-        }
-
-        var currentIndex = 0;
-        var current = _head;
-        while (current != null)
-        {
-            if (currentIndex == index - 1)
-            {
-                var newNode = new LnkNode<T>(value, current.Next);
-                newNode.Next = current.Next;
-                current.Next = newNode;
-                return;
-            }
-
-            current = current.Next;
-            currentIndex++;
-        }
+        throw new NotImplementedException();
     }
 
     // O(1)
-    public int Count() => 
-        _count;
-    
+    public int Count()
+    {
+        throw new NotImplementedException();
+    }
+
     // O(n)
     public T[] ToArray()
     {
-        if (_head == null)
-            return Array.Empty<T>();
-
-        var result = new List<T>();
-
-        var current = _head;
-        while (current != null)
-        {
-            result.Add(current.Value);
-            current = current.Next;
-        }
-
-        return result.ToArray();
+        throw new NotImplementedException();
     }
 }
