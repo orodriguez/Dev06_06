a796727e6edd1aae3270d82760a0162a4c6b4c51
LuloDev <ezequielcruz018@gmail.com>
2024-02-06 16:16:08 -0400

    Dev06- LnkList Solution

--- AAD/LnkList.cs
+++ AAD/LnkList.cs
@@ -1,10 +1,17 @@
+using System.Formats.Asn1;
+using System.Runtime.InteropServices.Marshalling;
+
 namespace AAD;
 
-public class LnkList<T> {
-    
+public class LnkList<T>
+{
+
     public static LnkList<T> From(params T[] values)
     {
-        throw new NotImplementedException();
+        var item = new LnkList<T>();
+        foreach (var value in values)
+            item.Add(value);
+        return item;
     }
 
     private LnkNode<T>? _head;
@@ -27,24 +34,69 @@ public class LnkList<T> {
     // O(1)
     public void Add(T element)
     {
-        throw new NotImplementedException();
+        var newitem = new LnkNode<T>(element);
+
+        if (_head == null)
+            _head = _last = newitem;
+        else
+        {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            _last.Next = newitem;
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+            _last = newitem;
+        }
+
+        _count++;
     }
 
     // O(n)
     public void Insert(int index, T value)
     {
-        throw new NotImplementedException();
+        if (_count == 0)
+            return;
+
+        if (index == 0)
+        {
+            var newitem = new LnkNode<T>(value, _head);
+            _head = newitem;
+            return;
+        }
+
+        var actual_index = 0;
+        var position = _head;
+        while (position != null)
+        {
+            if (actual_index == index - 1)
+            {
+                var newitem = new LnkNode<T>(value, position.Next);
+                newitem.Next = position.Next;
+                position.Next = newitem;
+                return;
+            }
+            position = position.Next;
+            actual_index++;
+        }
     }
 
     // O(1)
-    public int Count()
-    {
-        throw new NotImplementedException();
-    }
+    public int Count() =>
+    _count;
 
     // O(n)
     public T[] ToArray()
     {
-        throw new NotImplementedException();
+        if (_head == null)
+            return Array.Empty<T>();
+
+        var answers = new List<T>();
+
+        var position = _head;
+        while (position != null)
+        {
+            answers.Add(position.Value);
+            position = position.Next;
+        }
+
+        return answers.ToArray();
     }
 }
