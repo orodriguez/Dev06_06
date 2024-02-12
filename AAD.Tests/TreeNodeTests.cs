7e8d9a8fa0e4ee037701b05b20a8df852f63f2b5
“orodriguez” <omarjavier15@gmail.com>
2024-02-12 12:52:19 -0400

    TreeNodeTests.Count TreeNodeTests.Level

--- AAD.Tests/TreeNodeTests.cs
+++ AAD.Tests/TreeNodeTests.cs
@@ -8,12 +8,12 @@ public class TreeNodeTests
     public void Constructor()
     {
         var t = new TreeNode<string>("Super Market");
-        
+
         Assert.Equal("Super Market", t.Value);
         Assert.True(t.IsRoot);
         Assert.Empty(t.Children);
     }
-    
+
     [Fact]
     public void Add()
     {
@@ -22,10 +22,58 @@ public class TreeNodeTests
         superMarketNode.Add("Vegetables");
 
         var vegetablesNode = Assert.Single(superMarketNode.Children);
-        
+
         Assert.Equal("Vegetables", vegetablesNode.Value);
         Assert.Equal(superMarketNode, vegetablesNode.Parent);
         Assert.Empty(vegetablesNode.Children);
+    }
+
+    [Fact]
+    public void Count_OneNode()
+    {
+        var superMarketNode = new TreeNode<string>("Super Market");
+
+        Assert.Equal(1, superMarketNode.Count());
+    }
+
+    [Fact]
+    public void Count_Many()
+    {
+        var superMarket = new TreeNode<string>("Super Market");
+
+        var vegetables = superMarket
+            .Add("Vegetables");
+
+        vegetables.Add("Tomato");
+        vegetables.Add("Carrot");
+
+        Assert.Equal(4, superMarket.Count());
+    }
+
+    [Fact]
+    public void Level()
+    {
+        var superMarket = new TreeNode<string>("Super Market");
+
+        Assert.Equal(0, superMarket.Level());
+    }
+
+    [Fact]
+    public void Level_OneChild()
+    {
+        var child = new TreeNode<string>("A").Add("B");
+
+        Assert.Equal(1, child.Level());
+    }
+    
+    [Fact]
+    public void Level_Two()
+    {
+        var leaf = new TreeNode<string>("A")
+            .Add("B")
+            .Add("C");
+
+        Assert.Equal(2, leaf.Level());
     }
 }
 
@@ -41,12 +89,35 @@ public class TreeNode<T>
         Parent = parent;
         Children = new List<TreeNode<T>>();
     }
-    
+
     public bool IsRoot => Parent == null;
 
-    public void Add(T childValue) => 
+    public int Level()
+    {
+        var level = 0;
+        var current = this;
+        while (current.Parent != null)
+        {
+            level++;
+            current = current.Parent;
+        }
+
+        return level;
+    }
+
+    public TreeNode<T> Add(T childValue) =>
         Add(new TreeNode<T>(childValue, parent: this));
 
-    private void Add(TreeNode<T> node) => 
+    private TreeNode<T> Add(TreeNode<T> node)
+    {
         Children.Add(node);
+        return node;
+    }
+
+    public int Count() => ChildrenCount() + 1;
+
+    private int ChildrenCount() =>
+        Children
+            .Select(child => child.Count())
+            .Sum();
 }
