0d2c5de5e2daea4204691224d8609f07278aec3b
Ismael Varela <info@varelait.com>
2024-02-13 00:50:17 -0400

    day 2 copleted

--- AAD/DoublyLnkList.cs
+++ AAD/DoublyLnkList.cs
@@ -4,28 +4,57 @@ public class DoublyLnkList<T> where T : notnull
 {
     public static DoublyLnkList<T> From(params T[] values)
     {
-        var ll = new LnkList<T>();
+        var ll = new DoublyLnkList<T>();
         foreach (var value in values)
             ll.Add(value);
         return ll;
     }
 
-    private DoubleLnkNode<T>? _head;
+    private DoublyLnkNode<T>? _head;
 
-    private DoubleLnkNode<T>? _last;
+    private DoublyLnkNode<T>? _last;
 
-    private int _count;
+    private int _count = 0;
+
+
+    public T this[int index] => Get(index);
+
+    public T Get(int index)
+    {
+        if (_head == null)
+            throw new IndexOutOfRangeException();
+
+        if (index < 0 || index >= _count)
+            throw new IndexOutOfRangeException();
+
+        var currentNode = _head;
+        var currentIndex = 0;
+
+        while (currentNode != null)
+        {
+            if (currentIndex == index)
+                break;
+
+            currentIndex++;
+            currentNode = currentNode.Next;
+        }
+
+        return currentNode.Value;
+    }
 
 
 
-    public T this[int index] => throw new NotImplementedException();
 
     public void Prepend(T value)
     {
         if (_head == null)
-            _last = _head = new DoubleLnkNode<T>(value);
+            _last = _head = new DoublyLnkNode<T>(value);
         else
-            _head = new DubleLnkNode<T>(value, next: _head);
+        {
+            var newNode = new DoublyLnkNode<T>(value);
+            newNode.Next = _head;
+            _head = newNode;
+        }
             
         _count++;
         return;
@@ -35,36 +64,159 @@ public class DoublyLnkList<T> where T : notnull
 
     public void Add(T value)
     {
-        throw new NotImplementedException();
+        var newNode = new DoublyLnkNode<T>(value);
+
+        // O(1)
+        if (_head == null)
+            _head =_last = newNode;
+        else // O(1)
+        {
+            _last.Next = newNode;
+            newNode.Prev = _last;
+            _last = newNode;
+        }
+
+        _count++;
+        return;
     }
 
     public void Insert(int index, T value)
     {
-        throw new NotImplementedException();
+        // O(1)
+        if (_count == 0 || index < 0 || index >= _count)
+            return;
+
+        // O(1)
+        if (index == 0)
+        {
+            var newNode = new DoublyLnkNode<T>(value);
+            _head.Prev = newNode;
+            newNode.Next = _head;
+            _head = newNode;
+        }
+
+        var currentIndex = 1;
+        var current = _head.Next;
+        while (current != null)
+        {
+            if (currentIndex == index)
+            {
+                var newNode = new DoublyLnkNode<T>(value);
+                var previousNode = current.Prev;
+                previousNode.Next = newNode;
+                newNode.Next = current;
+                current.Prev = newNode;
+                newNode.Prev = previousNode;
+                break;
+            }
+
+            current = current.Next;
+            currentIndex++;
+        }    
+        
+        _count++;
+        return;
     }
 
     public bool Remove(T value)
     {
-        throw new NotImplementedException();
+        if (_head == null)
+            return false;
+
+        if (_head.ValueEquals(value))
+        {
+            _head = _head.Next;
+            _count--;
+            return true;
+        }
+
+        var currentNode = _head;
+        while (currentNode != null)
+        {
+            if (currentNode.NextValueEquals(value))
+            {
+                var nextNode = currentNode.Next;
+                currentNode.Next = nextNode!.Next;
+                _count--;
+                return true;
+            }
+
+            currentNode = currentNode.Next;
+        }
+
+        return false;
+
     }
 
     public void RemoveAt(int index)
     {
-        throw new NotImplementedException();
+        if (_head == null)
+            throw new IndexOutOfRangeException();
+
+        if (index < 0 || index >= _count)
+            throw new IndexOutOfRangeException();
+
+        if (index == 0)
+        {
+            _head = _head.Next;
+            return;
+        }
+        
+        var currentIndex = 0;
+        var currentNode = _head;
+        
+        while (currentNode != null)
+        {
+            if (currentIndex == index - 1)
+            {
+                currentNode.Next = currentNode.Next!.Next;
+                return;
+            }
+            
+            currentIndex++;
+            currentNode = currentNode.Next;
+        }
     }
 
     public T Last()
     {
-        throw new NotImplementedException();
+        if(_last == null)
+            throw new InvalidOperationException();
+
+        return _last.Value;
     }
 
     public T[] ToArray()
     {
-        throw new NotImplementedException();
+        if (_head == null)
+            return Array.Empty<T>();
+
+        var result = new List<T>();
+
+        var current = _head;
+        while (current != null)
+        {
+            result.Add(current.Value);
+            current = current.Next;
+        }
+
+        return result.ToArray();
     }
 
     public T[] ToReversedArray()
     {
-        throw new NotImplementedException();
+        if (_last == null)
+            return Array.Empty<T>();
+
+        var result = new List<T>();
+
+        var current = _last;
+        while (current != null)
+        {
+            result.Add(current.Value);
+            current = current.Prev;
+        }
+
+        return result.ToArray();
     }
 }
--- AAD/LnkNode.cs
+++ AAD/LnkNode.cs
@@ -20,12 +20,14 @@ public class LnkNode<T> where T : notnull
 
 
 
-public class DoubleLnkNode<T> where T : notnull
+public class DoublyLnkNode<T> where T : notnull
 {
     public T Value { get; }
-    public LnkNode<T>? Next { get; set; }
+    public DoublyLnkNode<T>? Next { get; set; }
+    public DoublyLnkNode<T>? Prev { get; set; }
 
-    public LnkNode(T value, LnkNode<T>? next = null, LnkNode<T>? pref = null)
+
+    public DoublyLnkNode(T value, DoublyLnkNode<T>? next = null, DoublyLnkNode<T>? prev = null)
     {
         Value = value;
         Next = next;
@@ -39,5 +41,5 @@ public class DoubleLnkNode<T> where T : notnull
         Next != null && Next.ValueEquals(value);
 
     public bool PreValueEquals(T value) => 
-        Next != null && Prev.ValueEquals(value);
+        Prev != null && Prev.ValueEquals(value);
 }
