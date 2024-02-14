53e2fda5b29b78a6034819eab0e60ed6b2dbe99e
“orodriguez” <omarjavier15@gmail.com>
2024-02-14 15:32:42 -0400

    Set assignment

---
+++ AAD/Set.cs
@@ -0,0 +1,19 @@
+namespace AAD;
+
+public class Set
+{
+    public void Add(int value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int[] ToArray()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(int value)
+    {
+        throw new NotImplementedException();
+    }
+}
