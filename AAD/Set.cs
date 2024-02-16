fc9c7a32cd0bb03b1458163d4c491c0ae0f34195
RicardoCollado <ricardocolladorothschild@gmail.com>
2024-02-16 00:04:43 -0400

    Implemented Set data structure: Use BStree as base data structure to hold data when saved into a Set.

--- AAD/Set.cs
+++ AAD/Set.cs
@@ -2,18 +2,37 @@ namespace AAD;
 
 public class Set
 {
+    private BSTreeNode bst_data;
+    private bool _flag = false; // to verify if first time adding
+    
+
     public void Add(int value)
     {
-        throw new NotImplementedException();
+        if (!_flag)
+        {
+            bst_data = new BSTreeNode(value);
+            this._flag = true;
+        }
+        else
+        {
+            
+            bst_data.Add(value);            
+
+
+        }
+        
     }
 
     public int[] ToArray()
     {
-        throw new NotImplementedException();
+        var result = new List<int>();
+        this.bst_data.TraverseInOrder(node => result.Add(node.Value));
+
+        return result.ToArray();
     }
 
     public bool Contains(int value)
     {
-        throw new NotImplementedException();
+        return this.bst_data.Contains(value); 
     }
 }
