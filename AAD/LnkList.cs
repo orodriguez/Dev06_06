ffb6cf5a93749031424ea9920f694f130d3f1473
Lary Figuereo <laryfiguereo@gmail.com>
2024-02-19 20:03:45 -0400

    link list done lary figuereo

--- AAD/LnkList.cs
+++ AAD/LnkList.cs
@@ -4,8 +4,14 @@ public class LnkList<T> {
     
     public static LnkList<T> From(params T[] values)
     {
-        throw new NotImplementedException();
-    }
+        var LinkList = new LnkList<T>();
+        foreach(var value in values){
+           // var linkNode = new LnkNode<T>(value); 
+            LinkList.Add(value);
+            
+        }
+        return LinkList;
+    }   
 
     private LnkNode<T>? _head;
 
@@ -27,24 +33,63 @@ public class LnkList<T> {
     // O(1)
     public void Add(T element)
     {
-        throw new NotImplementedException();
+        var newNode = new LnkNode<T>(element);
+        if(_head == null){
+            _head = newNode;
+            _last = _head;
+        }else{
+        _last.Next = newNode;
+        _last = newNode;
+        }
+        _count++;
     }
 
     // O(n)
     public void Insert(int index, T value)
     {
-        throw new NotImplementedException();
+        var newNode = new LnkNode<T>(value);
+        if(_head == null){
+            return;
+        }
+        if(index == 0){
+            newNode.Next = _head;
+            _head = newNode;
+        }else{
+            var iterator = 1;
+        var currentNode = _head;
+        while(iterator < _count){
+            if(index == iterator){
+                newNode.Next = currentNode.Next;
+                currentNode.Next = newNode;
+            }
+            currentNode = currentNode.Next;
+            iterator++;
+        }
+        }
+        _count++;
+        
     }
 
     // O(1)
     public int Count()
     {
-        throw new NotImplementedException();
+        return _count;
     }
 
     // O(n)
     public T[] ToArray()
     {
-        throw new NotImplementedException();
+        var dinamyctList = new List<T>();
+         if(_head == null)
+            return Array.Empty<T>();
+
+        var currentNode = _head;
+        
+        while(currentNode != null){
+            dinamyctList.Add(currentNode.Value);
+            currentNode = currentNode.Next;
+        }
+
+        return dinamyctList.ToArray();
     }
 }
